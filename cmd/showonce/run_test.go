ebc21aeb8a8fb66115fc6fc4952f6618eda52e46
Alexey Kovrizhkin <lekovr@gmail.com>
2023-07-29 16:49:47 +0300

    fix: security/code-scanning/3

--- cmd/showonce/run_test.go
+++ cmd/showonce/run_test.go
@@ -74,14 +74,16 @@ func GetPorts(t *testing.T) (int, int) {
 // Code from https://gist.github.com/sevkin/96bdae9274465b2d09191384f86ef39d
 
 // GetFreePort asks the kernel for a free open port that is ready to use.
-func GetFreePort() (port int, err error) {
+func GetFreePort() (int, error) {
 	var a *net.TCPAddr
+	var err error
 	if a, err = net.ResolveTCPAddr("tcp", "localhost:0"); err == nil {
 		var l *net.TCPListener
 		if l, err = net.ListenTCP("tcp", a); err == nil {
-			defer l.Close()
-			return l.Addr().(*net.TCPAddr).Port, nil
+			port := l.Addr().(*net.TCPAddr).Port
+			l.Close()
+			return port, err
 		}
 	}
-	return
+	return 0, err
 }
